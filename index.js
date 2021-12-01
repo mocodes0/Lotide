9731c319333ec5002b326d1bfc60371380e0228b
Mohamed Bashir <mo.dev416@gmail.com>
2021-12-01 02:21:12 +0000

    Implenting short hand syntax to index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// implemting shorthand syntax
+const head   = require('./head');
+const middle = require('./middle');
+const tail   = require('./tail');
+const eqArrays   = require('./eqArrays');
+const assertEqual = require('./assertEqual')
+const assertArraysEqual = require('./assertArraysEqual');
+
+
+module.exports = {
+  head,
+  middle,
+  tail,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+};
